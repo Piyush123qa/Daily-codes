661f274b0d505b9ba153d0bff8ab053e559d18ef
Piyush123qa <99413920+Piyush123qa@users.noreply.github.com>
2024-06-05 18:47:52 +0530

    Create workcarddata.js

---
+++ workcarddata.js
@@ -0,0 +1,19 @@
+import pro1 from "../Assets/proj1.jpg"
+import pro2 from "../Assets/proj2.jpg"
+
+const ProjectCardData = [
+    {
+        imagesrc: pro1,
+        title: "Portfolio",
+        text: "Welcome to my portfolio! This collection showcases my skills and projects where I've applied my expertise in CSS, JavaScript, and various APIs. Through these technologies, I've created dynamic, responsive, and interactive web experiences",
+        view: "Github link",
+    },
+    {
+        imagesrc: pro2,
+        title: "Text summarization of various language",
+        text: "This project focuses on developing a multilingual text summarization system capable of handling both Hindi and English texts.It demonstrates the effective use of machine learning to bridge language barriers and provide valuable insights through automated text summarization",
+        view: "Github link",
+    },
+];
+
+export default ProjectCardData;
